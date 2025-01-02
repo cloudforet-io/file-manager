d59188fa5799b5af146c74785a04c3af7b2bab53
kang2453 <kang2453@gmail.com>
2025-01-02 23:07:11 +0900

    feat: add MinIOS3Connector and UserFile service to file manager
    Signed-off-by: kang2453 <kang2453@gmail.com>

--- src/spaceone/file_manager/interface/grpc/__init__.py
+++ src/spaceone/file_manager/interface/grpc/__init__.py
@@ -1,7 +1,9 @@
 from spaceone.core.pygrpc.server import GRPCServer
 from spaceone.file_manager.interface.grpc.file import File
+from spaceone.file_manager.interface.grpc.user_file import UserFile
 
 _all_ = ["app"]
 
 app = GRPCServer()
 app.add_service(File)
+app.add_service(UserFile)
